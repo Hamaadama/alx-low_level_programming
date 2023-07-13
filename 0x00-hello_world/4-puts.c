2cc7766216c440aa90e3907ed722a33362bf4b7e
ADAM <hamadamahamidou@gmail.com>
2023-07-13 12:57:56 -0700

    Rectification

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,12 +1,12 @@
 #include<stdio.h>
 
 /**
- * main - Entry point
+ * main - Main funcition of c.
  *
- * Return: Always 0 (Success)
+ * Return: Always 0 (Succes).
  */
 int main(void)
 {
-	puts("\"Programming is like building a multilingual puzzle\n");
+	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
 }
