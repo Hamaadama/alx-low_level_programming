f00db2ddcd32e0200959a915801787bac27fd988
ADAM <hamadamahamidou@gmail.com>
2023-07-21 13:31:49 -0700

    Add the C program 4-print_alphabt

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+/**
+ * main - Main function of C.
+ *
+ * Return: Always 0 (Succes).
+ */
+int main(void)
+{
+	char lower;
+
+	for (lower = 'a'; lower <= 'z'; lower++)
+	{
+	if (lower != 'e' && lower != 'q')
+	putchar(lower);
+	}
+	putchar('\n');
+
+	return (0);
+}
