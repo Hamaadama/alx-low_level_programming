a6c495c2bf9f891029e3986d3dd29e9d51ea0b7a
ADAM <hamadamahamidou@gmail.com>
2023-07-21 13:40:05 -0700

    Add the C program 5-print_numbers

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+/**
+ * main - Main function of C.
+ *
+ * Return: Always 0 (Succes).
+ */
+int main(void)
+{
+	int number;
+
+	for (number = 0; number < 10; number++)
+	printf("%d", number);
+	printf("\n");
+
+	return (0);
+}
