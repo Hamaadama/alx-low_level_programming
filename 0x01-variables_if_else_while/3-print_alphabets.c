4948d3268eca8d8fe7afcafb9a67ff21126f0a93
ADAM <hamadamahamidou@gmail.com>
2023-07-21 13:22:57 -0700

    Add the C program 3-print_alphabets

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+/**
+ * main - Main function of C.
+ *
+ * Return: Always 0 (Succes).
+ */
+int main(void)
+{
+	char lower;
+	char upper;
+
+	for (lower = 'a'; lower <= 'z'; lower++)
+	putchar(lower);
+
+	for (upper = 'A'; upper <= 'Z'; upper++)
+	putchar(upper);
+	putchar('\n');
+
+	return (0);
+}
