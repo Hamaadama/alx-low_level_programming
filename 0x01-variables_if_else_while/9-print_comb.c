8cff514af779de36fe5a445ac0893688372a0e83
ADAM <hamadamahamidou@gmail.com>
2023-07-21 14:14:32 -0700

    Add the C program 9-print_comb.c

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+/**
+ * main - Main function of C.
+ *
+ * Return: Always 0 (Succes).
+ */
+int main(void)
+{
+	int number;
+
+	for (number = '0'; number <= '9'; number++)
+	{
+	putchar(number);
+	if (number != '9')
+	{
+	putchar(',');
+	putchar(' ');
+	}
+	}
+	putchar('\n');
+
+	return (0);
+}
