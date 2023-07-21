8f6a19a9a1a5b60ebeeeaaf2fb5170c7a878e099
ADAM <hamadamahamidou@gmail.com>
2023-07-21 13:15:56 -0700

    Add the C program 2-print_alphabet

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+/**
+ * main - Main function of C.
+ *
+ * Return: Always 0 (Succes).
+ */
+int main(void)
+{
+	char c;
+
+	for (c = 'a'; c <= 'z'; c++)
+	putchar(c);
+	putchar('\n');
+
+	return (0);
+}
