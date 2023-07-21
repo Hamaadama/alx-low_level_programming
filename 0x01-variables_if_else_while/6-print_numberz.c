b6760f6ce126b860ed23953d541e37947fcf70cb
ADAM <hamadamahamidou@gmail.com>
2023-07-21 13:47:59 -0700

    Add the C program 6-print_numberz

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+/**
+ * main - Main function of C.
+ *
+ * Return: Always 0 (Succes).
+ */
+int main(void)
+{
+	int number;
+
+	for (number = '0'; number <= '9'; number++)
+	putchar(number);
+	putchar('\n');
+
+	return (0);
+}
