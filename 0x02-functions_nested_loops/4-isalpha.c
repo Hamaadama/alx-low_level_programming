244cb2188f1521691a09c899f43c2747ce67b727
Hama ADAMA <92546875+Hamaadama@users.noreply.github.com>
2023-07-25 00:55:26 +0300

    Update 4-isalpha.c

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -4,7 +4,7 @@
  * _isalpha - checks for alphabetic character
  * @c: character to be checked
  *
- * Return: 1 if c is a letter, 0 otherwise
+ * Return: 1 if character is a letter, 0 otherwise
  */
 int _isalpha(int c)
 {
