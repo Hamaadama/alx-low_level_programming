f91400b7712859d50b743ce8bdd61e6c518c62aa
ADAM <hamadamahamidou@gmail.com>
2023-07-24 19:21:32 +0000

    Modify the C file 7-print_last_digit

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -7,10 +7,13 @@
  */
 int print_last_digit(int number)
 {
-int last_digit;
-	if (number < 0)
-		number = -number;
+	int last_digit;
+
 	last_digit = number % 10;
+	if (last_digit < 0)
+	{
+		last_digit = last_digit * -1;
+	}
 	_putchar(last_digit + '0');
 	return (last_digit);
 }
