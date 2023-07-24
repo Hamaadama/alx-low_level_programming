480652ea11d98a09d4e636b34c24f60dcae9510a
ADAM <hamadamahamidou@gmail.com>
2023-07-24 18:57:14 +0000

    Add the C program 6-abs

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * _abs - return absolute value of a number
+ * @number: number to be checked
+ *
+ * Return: the absolute value of given int.
+ */
+int _abs(int number)
+{
+	if (number >= 0)
+	{
+		return (number);
+	}
+	return (-number);
+}
