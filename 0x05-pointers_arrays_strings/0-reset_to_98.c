b4658ec31bbaecb21c6ad926bba2a6814b3a4cca
ADAM <hamadamahamidou@gmail.com>
2023-07-25 13:13:10 -0700

    Add 0-main.c and 0-reset_to_98.c

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,11 @@
+#include "main.h"
+
+/**
+ * reset_to_98 - reset the value of n to 98
+ * @n: number to be considered for reseting
+ * Return: void, nothing just the printing
+ */
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
