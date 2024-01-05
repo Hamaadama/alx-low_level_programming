1dd67aa29230cc34e3986896176f4ec9699975a5
ADAM <proselflearner@gmail.com>
2024-01-05 18:56:27 +0300

    Add 6-main.c and 6-cap_string.c

---
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,39 @@
+#include "main.h"
+
+/**
+ * cap_string - Capitalizes all words of a string.
+ *
+ * @str: The input string.
+ *
+ * Return: A pointer to the modified string.
+ */
+
+char *cap_string(char *str)
+{
+	int i = 0;
+
+	while (str[i] != '\0')
+	{
+		if (i == 0 || str[i - 1] == ' ' || str[i - 1] == '\t' || str[i - 1] == '\n'
+			|| str[i - 1] == ',' || str[i - 1] == ';' || str[i - 1] == '.'
+			|| str[i - 1] == '!' || str[i - 1] == '?' || str[i - 1] == '"'
+			|| str[i - 1] == '(' || str[i - 1] == ')' || str[i - 1] == '{'
+			|| str[i - 1] == '}')
+		{
+			if (str[i] >= 'a' && str[i] <= 'z')
+			{
+				str[i] = str[i] - 'a' + 'A';
+			}
+		}
+		else
+		{
+			if (str[i] >= 'A' && str[i] <= 'Z')
+			{
+				str[i] = str[i] + 'a' - 'A';
+			}
+		}
+		i++;
+	}
+
+	return (str);
+}
