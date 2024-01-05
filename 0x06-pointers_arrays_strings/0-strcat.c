e8e5bab92687f40a0cafb00931e6176bbca3f65e
ADAM <proselflearner@gmail.com>
2024-01-05 19:20:27 +0300

    Do some update

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -2,32 +2,28 @@
 
 /**
  * _strcat - Concatenates two strings.
- * @dest: The destination string to which 'src' is appended.
- * @src: The source string to append to 'dest'.
- *
- * Return: A pointer to the resulting string 'dest'.
+ * @dest: Destination.
+ * @src: Source.
+ * Return: The pointer to dest.
  */
 
 char *_strcat(char *dest, char *src)
 {
-	int dest_index = 0, src_index = 0;
+	int count = 0, count2 = 0;
 
-	/* Find the end of the destination string */
-	while (dest[dest_index] != '\0')
+	while (*(dest + count) != '\0')
 	{
-		dest_index++;
+		count++;
 	}
 
-	/* Append the source string to the destination string */
-	while (src[src_index] != '\0')
+	while (count2 >= 0)
 	{
-		dest[dest_index] = src[src_index];
-		dest_index++;
-		src_index++;
+		*(dest + count) = *(src + count2);
+		if (*(src + count2) == '\0')
+			break;
+		count++;
+		count2++;
 	}
-
-	/* Add the terminating null byte to the destination string */
-	dest[dest_index] = '\0';
 
 	return (dest);
 }
