1477ccac6fdfd8149e61eae31ddb41c6c960cf5e
ADAM <proselflearner@gmail.com>
2024-01-05 19:50:36 +0300

    Add 100-main.c and 100-rot13.c

---
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -0,0 +1,29 @@
+#include "main.h"
+
+/**
+ * rot13 - Encodes a string using ROT13.
+ * @str: The string to be encoded.
+ *
+ * Return: A pointer to the resulting encoded string.
+ */
+
+char *rot13(char *str)
+{
+	char alphabet[] = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+	char rot13[] = "NOPQRSTUVWXYZABCDEFGHIJKLMnopqrstuvwxyzabcdefghijklm";
+	int i, j;
+
+	for (i = 0; str[i] != '\0'; i++)
+	{
+		for (j = 0; alphabet[j] != '\0'; j++)
+		{
+			if (str[i] == alphabet[j])
+			{
+				str[i] = rot13[j];
+				break;
+			}
+		}
+	}
+
+	return (str);
+}
